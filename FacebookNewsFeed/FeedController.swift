eca444917e693ca70d1f87319d1bea4c917de7a8
Milind Satam <milind.ms14@gmail.com>
2018-01-15 21:12:47 +0530

    Online Image

--- FacebookNewsFeed/FeedController.swift
+++ FacebookNewsFeed/FeedController.swift
@@ -8,13 +8,55 @@
 
 import UIKit
 
+class Post {
+    var name: String?
+    var statusText: String?
+    var profileImageName: String?
+    var statusImageName: String?
+    var statusImageUrl: String?
+    var numLikes: Int?
+    var numComments: Int?
+}
+
 class FeedController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     let cellId = "cellId"
+    
+    var posts = [Post]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let postMark = Post()
+        postMark.name = "Mark Zuckerberg"
+        postMark.statusText = "Meanwhile, beast turned to the dark side"
+        postMark.profileImageName = "zuck_image"
+        postMark.statusImageName = "zuckdog"
+        postMark.numLikes = 23
+        postMark.numComments = 5
+        postMark.statusImageUrl = "https://www.techworm.net/wp-content/uploads/2017/02/facebookmarl.jpg"
+        posts.append(postMark)
+        
+        let postSteve = Post()
+        postSteve.name = "Steve Jobs"
+        postSteve.statusText = "Being the richest man in the cemetery doesn't matter to me. Going to bed at night saying we've done something wonderful, that's what matters to me."
+        postSteve.profileImageName = "stevejobs"
+        postSteve.statusImageName = "sj_post"
+        postSteve.numLikes = 567
+        postSteve.numComments = 34
+        postSteve.statusImageUrl = "https://www.billboard.com/files/media/Steve-Jobs-2007-iphone-billboard-1548.jpg"
+        posts.append(postSteve)
+        
+        let postGandhi = Post()
+        postGandhi.name = "Mahatma Gandhi"
+        postGandhi.statusText = "You must not lose faith in humanity. Humanity is an ocean; if a few drops of the ocean are dirty, the ocean does not become dirty. Strength does not come from physical capacity. It comes from an indomitable will. The weak can never forgive. Forgiveness is the attribute of the strong."
+        postGandhi.profileImageName = "gandhi"
+        postGandhi.statusImageName = "mg_post"
+        postGandhi.numLikes = 13045
+        postGandhi.numComments = 456
+        postGandhi.statusImageUrl = "https://i.ndtvimg.com/i/2017-10/mahatma-gandhi_650x400_81506923735.jpg"
+        posts.append(postGandhi)
         
         collectionView?.backgroundColor = UIColor(white: 0.95, alpha: 1)
         collectionView?.alwaysBounceVertical = true
@@ -24,15 +66,27 @@ class FeedController: UICollectionViewController, UICollectionViewDelegateFlowLa
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        let feedCell =  collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! FeedCell
+        
+        feedCell.post = posts[indexPath.row]
+        
+        return feedCell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 400)
+        if let statusText = posts[indexPath.row].statusText {
+            let size = CGSize(width: view.frame.width, height: 1000)
+            let rect = NSString(string: statusText).boundingRect(with: size, options: NSStringDrawingOptions.usesFontLeading.union(NSStringDrawingOptions.usesLineFragmentOrigin), attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 14)], context: nil)
+            
+            let knownHeight: CGFloat = 8 + 44 + 4 + 4 + 200 + 8 + 24 + 8 + 44
+            
+            return CGSize(width: view.frame.width, height: rect.height + knownHeight + 24)
+        }
+        return CGSize(width: view.frame.width, height: 500)
     }
     
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
@@ -42,129 +96,6 @@ class FeedController: UICollectionViewController, UICollectionViewDelegateFlowLa
 
 }
 
-class FeedCell: UICollectionViewCell {
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        setupViews()
-    }
-    
-    let nameLabel: UILabel = {
-       let label = UILabel()
-        label.numberOfLines = 2
-        let attributedText = NSMutableAttributedString(string: "Mark Zuckerberg", attributes: [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 14)])
-        attributedText.append(NSAttributedString(string: "\nDecember 18 • San Francisco • ", attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 12), NSAttributedStringKey.foregroundColor: UIColor.rgb(red: 155, greeen: 161, blue: 171)]))
-        
-        let paragraphtStyle = NSMutableParagraphStyle()
-        paragraphtStyle.lineSpacing = 4
-        
-        attributedText.addAttribute(NSAttributedStringKey.paragraphStyle, value: paragraphtStyle, range: NSMakeRange(0, attributedText.length))
-        
-        
-        let attachment = NSTextAttachment()
-        attachment.image = UIImage(named: "earth_globe")
-        attachment.bounds = CGRect(x: 0, y: -2, width: 12, height: 12)
-        attributedText.append(NSAttributedString(attachment: attachment))
-        
-        label.attributedText = attributedText
-        return label
-    }()
-    
-    let profileImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(named: "zuck_image")
-        imageView.layer.cornerRadius = 22
-        imageView.layer.masksToBounds = true
-        return imageView
-    }()
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    let statusTextView: UITextField = {
-        let textField = UITextField()
-        textField.text = "Some random text for status goes here"
-        textField.font = UIFont.systemFont(ofSize: 14)
-        return textField
-    }()
-    
-    let statusImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "zuckdog")
-        imageView.contentMode = .scaleAspectFill
-        imageView.layer.masksToBounds = true
-        return imageView
-    }()
-    
-    let likesCommentsLabel: UILabel = {
-        let label = UILabel()
-        label.text = "488 Likes     34 Comments"
-        label.font = UIFont.systemFont(ofSize: 12)
-        label.textColor = UIColor.rgb(red: 155, greeen: 161, blue: 171)
-        return label
-    }()
-    
-    let dividerLineView: UIView = {
-        let view = UIView()
-        view.backgroundColor = UIColor.rgb(red: 226, greeen: 228, blue: 232)
-        return view
-    }()
-    
-    let likeButton = FeedCell.buttonForTitle(title: "Like", imageName: "like")
-    let commentButton = FeedCell.buttonForTitle(title: "Comment", imageName: "comment")
-    let shareButton = FeedCell.buttonForTitle(title: "Share", imageName: "share")
-    
-    static func buttonForTitle(title: String, imageName: String) -> UIButton {
-        let button = UIButton()
-        button.setTitle(title, for: .normal)
-        button.setTitleColor(UIColor.rgb(red: 143, greeen: 150, blue: 163), for: .normal)
-        button.titleEdgeInsets = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 0)
-        button.setImage(UIImage(named: imageName), for: .normal)
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
-        return button
-    }
-    
-    func setupViews() {
-        backgroundColor = UIColor.white
-        
-        addSubview(nameLabel)
-        addSubview(profileImageView)
-        addSubview(statusTextView)
-        addSubview(statusImageView)
-        addSubview(likesCommentsLabel)
-        addSubview(dividerLineView)
-        
-        addSubview(likeButton)
-        addSubview(commentButton)
-        addSubview(shareButton)
-        
-        
-        addConstraintsWithFormat(format: "H:|-8-[v0(44)]-8-[v1]-8-|", views: profileImageView, nameLabel)
-        
-        addConstraintsWithFormat(format: "H:|-8-[v0]-8-|", views: statusTextView)
-        
-        addConstraintsWithFormat(format: "H:|[v0]|", views: statusImageView)
-        
-        addConstraintsWithFormat(format: "H:|-12-[v0]-12-|", views: likesCommentsLabel)
-        
-        addConstraintsWithFormat(format: "H:|-12-[v0]-12-|", views: dividerLineView)
-        
-        addConstraintsWithFormat(format: "H:|[v0(v1)][v1(v2)][v2]|", views: likeButton, commentButton, shareButton)
-        
-        addConstraintsWithFormat(format: "V:|-12-[v0]", views: nameLabel)
-        
-        addConstraintsWithFormat(format: "V:|-8-[v0(44)]-4-[v1(30)]-4-[v2]-8-[v3(24)]-8-[v4(0.5)][v5(44)]|", views: profileImageView, statusTextView, statusImageView, likesCommentsLabel, dividerLineView, likeButton)
-        
-        addConstraintsWithFormat(format: "V:[v0(44)]|", views: commentButton)
-        addConstraintsWithFormat(format: "V:[v0(44)]|", views: shareButton)
-        
-    }
-    
-
-}
 
 extension UIColor {
     static func rgb(red: CGFloat, greeen: CGFloat, blue: CGFloat) -> UIColor {
